d4584f29cb4dcb8a1a9a35b1227061b007b987d6
apshaiTerp <apshaiTerp@yahoo.com>
2016-10-24 15:11:07 -0500

    New query approach

--- src/main/java/com/ac/umkc/spark/SparkDriver.java
+++ src/main/java/com/ac/umkc/spark/SparkDriver.java
@@ -1,5 +1,7 @@
 package com.ac.umkc.spark;
 
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -23,10 +25,27 @@ public class SparkDriver {
           .getOrCreate();
       
       Dataset<Row> df = spark.read().json(args[0]);
-      
-      df.show();
-
+      //df.show();
       df.printSchema();
+      
+      //New Approach for RDD
+      JavaRDD<TwitterUser> userRDD = spark.read().textFile(args[0]).javaRDD().map(
+          new Function<String, TwitterUser>() {
+            /** It wants it, so I gave it one */
+            private static final long serialVersionUID = 5654145143753968626L;
+
+            public TwitterUser call(String line) throws Exception {
+              TwitterUser user = new TwitterUser();
+              user.parseFromJSON(line);
+              return user;
+            }
+            
+          });
+      Dataset<Row> userDF = spark.createDataFrame(userRDD, TwitterUser.class);
+      userDF.createOrReplaceTempView("users");
+      
+      Dataset<Row> resultsDF = spark.sql("SELECT userType, count(*) FROM users GROUP BY userType");
+      resultsDF.show();
 
     } catch (Throwable t) {
       t.printStackTrace();
---
+++ src/main/java/com/ac/umkc/spark/UserType.java
@@ -0,0 +1,9 @@
+package com.ac.umkc.spark;
+
+/**
+ * @author AC010168
+ *
+ */
+public enum UserType {
+  DESIGNER, PUBLISHER, REVIEWER, CONVENTION, COMMUNITY;
+}
