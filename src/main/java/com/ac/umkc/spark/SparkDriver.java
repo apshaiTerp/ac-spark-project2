6127a257ff38ea50d6d511718c21af6592c3cfbd
apshaiTerp <apshaiTerp@yahoo.com>
2016-10-29 16:48:28 -0500

    Minor tweaks to command line UI

--- src/main/java/com/ac/umkc/spark/SparkDriver.java
+++ src/main/java/com/ac/umkc/spark/SparkDriver.java
@@ -122,7 +122,7 @@ public class SparkDriver implements Serializable {
         executeQuery4(hashTag, startDate, endDate);
       } else if (choice.equalsIgnoreCase("5")) {
         System.out.print ("Please enter a Search Term (You may include HashTags): ");
-        final String searchTerm = reader.next();
+        final String searchTerm = reader.nextLine();
         System.out.print ("How many results do you want returned: ");
         final int rowLimit = reader.nextInt();
         executeQuery5(searchTerm, rowLimit);
